e9529e86ec846ee9d984dc15224d8cdd97699ffb
PSJ <psj5329@naver.com>
2021-03-22 19:27:34 +0900

    [PSJ] 210322 메인 옵션 제외, 패스파인더 하는 중

--- Unrailed/Unrailed/MainScene.cpp
+++ Unrailed/Unrailed/MainScene.cpp
@@ -1,18 +1,86 @@
 #include "pch.h"
 #include "MainScene.h"
 
+#include "Image.h"
+#include "Button.h"
+
 void MainScene::Init()
 {
+	mBackground = IMAGEMANAGER->FindImage(L"MainScene");
+	mOptionWindow = IMAGEMANAGER->FindImage(L"OptionWindow");
+	Image* StartButton = IMAGEMANAGER->FindImage(L"StartButton");
+	Image* OptionButton = IMAGEMANAGER->FindImage(L"OptionButton");
+	Image* ExitButton = IMAGEMANAGER->FindImage(L"ExitButton");
+	Image* XButton = IMAGEMANAGER->FindImage(L"XButton");
+
+	mStartButton = new Button(StartButton, WINSIZEX / 2, WINSIZEY / 2, StartButton->GetWidth(), StartButton->GetHeight(), bind(&MainScene::StartButton, this));
+	mOptionButton = new Button(OptionButton, WINSIZEX / 2, WINSIZEY / 2 + 120, OptionButton->GetWidth(), OptionButton->GetHeight(), bind(&MainScene::OptionButton, this));
+	mExitButton = new Button(ExitButton, WINSIZEX / 2, WINSIZEY / 2 + 240, ExitButton->GetWidth(), ExitButton->GetHeight(), bind(&MainScene::ExitButton, this));
+	mXButton = new Button(XButton, WINSIZEX / 2 + 460, 75, XButton->GetWidth(), XButton->GetHeight(), bind(&MainScene::XButton, this));
+
+	mIsShowWindow = false;
 }
 
 void MainScene::Release()
 {
+	SafeDelete(mStartButton);
+	SafeDelete(mOptionButton);
+	SafeDelete(mExitButton);
+	SafeDelete(mXButton);
 }
 
 void MainScene::Update()
 {
+	if (!mIsShowWindow)
+	{
+		if (mStartButton != nullptr)
+			mStartButton->Update();
+		if (mOptionButton != nullptr)
+			mOptionButton->Update();
+		if (mExitButton != nullptr)
+			mExitButton->Update();
+	}
+	else
+	{
+		if (mXButton != nullptr)
+			mXButton->Update();
+	}
 }
 
 void MainScene::Render(HDC hdc)
 {
+	mBackground->ScaleRender(hdc, 0, 0, mBackground->GetWidth(), mBackground->GetHeight());
+	mStartButton->Render(hdc);
+	mOptionButton->Render(hdc);
+	mExitButton->Render(hdc);
+
+	if (mIsShowWindow)
+	{
+		mOptionWindow->ScaleRender(hdc, 140, 35, mOptionWindow->GetWidth(), mOptionWindow->GetHeight());
+		mXButton->Render(hdc);
+	}
+}
+
+void MainScene::StartButton()
+{
+	// 우선은 씬 넘어가자
+	// 나중에 창 띄워서 버트ㅡㄴ 할거얌
+	SCENEMANAGER->LoadScene(L"Scene1");
+}
+
+void MainScene::OptionButton()
+{
+	if (!mIsShowWindow)
+		mIsShowWindow = true;
+}
+
+void MainScene::ExitButton()
+{
+	PostQuitMessage(0);
+}
+
+void MainScene::XButton()
+{
+	if (mIsShowWindow)
+		mIsShowWindow = false;
 }
--- Unrailed/Unrailed/PathFinder.h
+++ Unrailed/Unrailed/PathFinder.h
@@ -1,13 +1,41 @@
 #pragma once
 
-struct Load {
-	int x;				// ㅇ
-	int y;
-	bool isPass;		// 지나간 길인지 확인... 그냥 벡터하지말까
-};
 
+class Tile;
 class PathFinder
 {
-	vector<Load>* 
-};
+	Singleton(PathFinder)
 
+public:
+	struct Load {
+		int x;				// 타일 인덱스
+		int y;
+		bool isPass;		// 지나간 길인지 확인... 그냥 벡터하지말까
+	};
+
+private:
+	vector<vector<Tile*>> mTileList;
+	vector<Load*> mFindPath;	// 이건 길 찾는 용도
+
+	int mStartIndexX;
+	int mStartIndexY;
+	int mArrivalIndexX;
+	int mArrivalIndexY;
+
+public:
+	void Init();
+	void FindPath();
+
+public:
+	vector<Load*> GetPath() { return mFindPath; }
+	int GetStartIndexX() { return mStartIndexX; }
+	int GetStartIndexY() { return mStartIndexY; }
+	int GetArrivalIndexX() { return mArrivalIndexX; }
+	int GetArrivalIndexY() { return mArrivalIndexY; }
+
+	void SetTileList(vector<vector<Tile*>> tile) { mTileList = tile; }
+	void SetStartIndexX(int x) { mStartIndexX = x; }
+	void SetStartIndexY(int y) { mStartIndexY = y; }
+	void SetArrivalIndexX(int x) { mArrivalIndexX = x; }
+	void SetArrivalIndexY(int y) { mArrivalIndexY = y; }
+};
